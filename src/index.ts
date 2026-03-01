de03b5aaf2c84a7aa4b358bce7b596cb88a899f4
jorduino <jordanhabash@gmail.com>
2026-03-01 06:08:36 +0000

    feat: make program generate schedule.json when it's missing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import * as fs from "node:fs/promises";
+import * as fs from "node:fs/promises"; // Use fs/promises for async operations
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 import Schedule from "./schedule/schedule";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const scheduleLocation = "./schedule.json";
 const employees = ["Employee 1", "Employee 3", "Employee 2"];
 const locations = ["Location 1", "Location 2", "Location 3"];
 const tzCachePath = "./cache.json";
+
 let schedule: Schedule;
 
 // if there is no schedule, tell the user to make one and exit
 if (!(await fs.exists(scheduleLocation))) {
-	console.error("schedule.json not found! Please make one using the following format:");
-	console.log(JSON.stringify(new Schedule(), null, 2));
+	await fs.writeFile(scheduleLocation, JSON.stringify(new Schedule(), null, 2));
+	const scheduleFullLocation = join(__dirname, scheduleLocation);
+	console.error(
+		`ERROR: schedule.json not found! Creating template, please modify '${scheduleFullLocation}'.\nRun program again once data is in schedule.json`,
+	);
 	process.exit();
 }
 
